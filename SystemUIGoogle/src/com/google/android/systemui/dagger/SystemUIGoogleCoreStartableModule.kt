abe864ba8ad02817f6da9a8af5d5c39a5abf76b8
spezi77 <spezi7713@gmx.net>
2022-10-16 14:21:36 +0200

    SystemUIGoogle: Use our ThemeOverlayController instead of Aosp.
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleCoreStartableModule.kt
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleCoreStartableModule.kt
@@ -44,6 +44,7 @@ import com.android.systemui.util.leak.GarbageMonitor
 import com.android.systemui.volume.VolumeUI
 import com.android.systemui.wmshell.WMShell
 import com.google.android.systemui.GoogleServices
+import com.cherish.android.systemui.theme.ThemeOverlayControllerCherish;
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.ClassKey
@@ -175,7 +176,7 @@ abstract class SystemUIGoogleCoreStartableModule {
     @Binds
     @IntoMap
     @ClassKey(ThemeOverlayController::class)
-    abstract fun bindThemeOverlayController(sysui: ThemeOverlayController): CoreStartable
+    abstract fun bindThemeOverlayControllerCherish(sysui: ThemeOverlayControllerCherish): CoreStartable
 
     /** Inject into ToastUI.  */
     @Binds
